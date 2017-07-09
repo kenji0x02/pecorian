d7aad4ef5be265fa09415eaa5310df5e990c0c29
Kenji <kenji0x02@gmail.com>
2017-07-10 00:45:34 +0900

    Modify git repository scope

--- pecorian.d/git_repository.sh
+++ pecorian.d/git_repository.sh
@@ -2,9 +2,9 @@ pecorian_git_repository_cmd() {
 
   # 2) select target
   local target="$( ghq list | pip_peco target )"
+  # common process
   local post_command=""
-  # ここ共通
-  [ -z "$target" ] && exit 1
+  [ -z "$target" ] && pecorian_abort
 
   # 3) select action
   local action_list
@@ -13,14 +13,16 @@ pecorian_git_repository_cmd() {
   action_list=(${action_list[@]} "cd && open with explorer")
   action_list=(${action_list[@]} "cd && git pull origin master")
   action_list=(${action_list[@]} "open with browser")
-  # ここ共通(actionは必ず配列から選択するので)
+  # common process ("action" is always selected from array)
   local action="$(for e in ${action_list[@]}; do echo $e; done | pip_peco action )"
-  [ -z "$action" ] && exit 1
+  [ -z "$action" ] && pecorian_abort
 
+  # 4) create command
   if [ $action = "cd && open with explorer" ]; then
     local ghq_root=""
-    if [ "$COMSPEC" != "" ]; then
-      ghq_root="$( cygpath `ghq root` )" # Windowsの場合はスラッシュ表記に変更
+    if pecorian_is_windows_os; then
+      # Windowsの場合はスラッシュ表記に変更
+      ghq_root="$( cygpath `ghq root` )"
       post_command="&& explorer ."
     else
       ghq_root="`ghq root`"
@@ -30,8 +32,8 @@ pecorian_git_repository_cmd() {
     action="cd"
   elif [ $action = "cd && git pull origin master" ]; then
     local ghq_root=""
-    if [ "$COMSPEC" != "" ]; then
-      ghq_root="$( cygpath `ghq root` )" # Windowsの場合はスラッシュ表記に変更
+    if pecorian_is_windows_os; then
+      ghq_root="$( cygpath `ghq root` )"
     else
       ghq_root="`ghq root`"
     fi
@@ -40,8 +42,8 @@ pecorian_git_repository_cmd() {
     post_command="&& git pull origin master"
   elif [ $action = "cd" ]; then
     local ghq_root=""
-    if [ "$COMSPEC" != "" ]; then
-      ghq_root="$( cygpath `ghq root` )" # Windowsの場合はスラッシュ表記に変更
+    if pecorian_is_windows_os; then
+      ghq_root="$( cygpath `ghq root` )"
     else
       ghq_root="`ghq root`"
     fi
@@ -52,7 +54,7 @@ pecorian_git_repository_cmd() {
     action="hub browse"
   fi
 
-  # 4) return command
+  # 5) return command
   echo "${action} ${target} ${post_command}"
 
   return
